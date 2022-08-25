e4a2718c5020f71fed7b016c8ba611d499c107a0
Wzyan <3101885298@qq.com>
2022-08-26 07:41:51 +0800

    feat: 执行视频下载任务

--- all.sh
+++ all.sh
@@ -48,6 +48,10 @@ echo '4. 爬虫生成配置目录完成'
 echo '5. 启动 ======'
 npm run build
 npm run build1
+echo '下载课件目录完成 ====== 开始执行视频下载任务'
+npm run build-sh
+echo '下载课件目录完成 ====== 执行视频下载任务完成'
+
 # echo '5.1 生成静态资源目录完成'
 # npm run build
 # npm run build2

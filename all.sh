b8ec14441fad83dce99a53333f997c9784cc0053
Wzyan <3101885298@qq.com>
2022-08-26 19:28:03 +0800

    feat: 实现脚本进行安装1

--- all.sh
+++ all.sh
@@ -46,10 +46,7 @@ npm run formatConfig
 echo '4. 爬虫生成配置目录完成'
 # 启动
 echo '5. 启动 ======'
-npm run build
-npm run build1
-echo '下载课件目录完成 ====== 开始执行视频下载任务'
-npm run build-sh
+npm run run-linux
 echo '下载课件目录完成 ====== 执行视频下载任务完成'
 
 # echo '5.1 生成静态资源目录完成'

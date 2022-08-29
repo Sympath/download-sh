923f1b8f060427400007ba4ec57debbb9bc79fc4
Wzyan <3101885298@qq.com>
2022-08-29 18:08:38 +0800

    feat: 添加脚本执行 chmod 1

--- config-retry.sh
+++ config-retry.sh
@@ -4,10 +4,6 @@
 while read line; do
     eval "$line"
 done <config
-if [ ! -d "/repo" ]; then
-    # 1. 先克隆仓库
-    git clone git@github.com:Sympath/kkb-download.git repo
-fi
 # 2. 切换进根目录
 cd repo
 echo '1. 切换进根目录完成'
